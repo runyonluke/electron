f218ce67f2716dc5f0cbe938cd835a0897bec94a
Luke Runyon <lrunyon@ara.com>
2025-01-09 08:44:47 -0600

    V 1.1.2 - Read operations part 2

--- renderer.js
+++ renderer.js
@@ -10,5 +10,39 @@ function fetchNotes() {
 }
 
 window.electronAPI.readNotes((rows) => {
-  console.log(rows);
+  rows.forEach((element) => {
+    div = returnNotesDropdown(element);
+    $("#accordion").append(div);
+  });
 });
+
+function returnNotesDropdown(element) {
+  return `<div class="panel panel-default" id="panel${element.id}">
+          <div class="panel-heading">
+            <span class="panel-title">
+              <a
+                data-toggle="collapse"
+                data-parent="#accordion"
+                href="#collapse${element.id}"
+                >${element.title}</a
+              >
+            </span>
+            <button
+              type="button"
+              class="btn btn-info act-btns"
+              data-toggle="modal"
+              data-target="#myModal"
+            >
+              <span class="glyphicon glyphicon-pencil"></span>
+            </button>
+            <button type="button" class="btn btn-danger act-btns">
+              <span class="glyphicon glyphicon-trash"></span>
+            </button>
+          </div>
+          <div id="collapse${element.id}" class="panel-collapse collapse">
+            <div class="panel-body">
+              ${element.description}
+            </div>
+          </div>
+        </div>`;
+}
